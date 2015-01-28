f32c405931817bcc1e49723a33c41dfd6b5fddc8
stephenmichael <stephenmichael30@gmail.com>
2015-01-28 14:00:03 +0000

    Update BaseController.php

--- system/core/BaseController.php
+++ system/core/BaseController.php
@@ -4,58 +4,58 @@
  * BaseController
  *
  * @author      Stephen Michael <info@stephenmichael.net>
+ * @copyright   Copyright (C), 2015 Stephen Michael
  * @version     1.0
- * 
  */
 
 class BaseController
 {
-	protected $_view;
+    protected $_view;
 	
     /**
-    * __construct - Required
-    */
-    public function __construct() 
-    {
-		$this->_view = new View();
-    }
+     * __construct
+     */
+     public function __construct() 
+     {
+         $this->_view = new View();
+     }
 	
     // ------------------------------------------------------------------------
 	
     /**
      * loadModel - Loads a Model
-	   * 
+     *
      * @param string $modelName - The name of the Model
-	   * @param string $area - The Area where the Model is located
-     * @return \fcr\app\$area\models
+     * @param string $area - The Area where the Model is located
+     * @return \app\$area\models
      */
-	public function loadModel($modelName, $area = false)
-	{
-		// Create the modelPath
-		$modelPath = '../app/';
-		
-		// Check if an Area has been defined
-		if ($area)
-		{
-			// Create the modelPath
-		    $modelPath .= 'areas/' . $area . '/';
-		}
-		
-		// Create the modelPath
-		$modelPath .= 'models/'. $modelName . '.php';
-		
-		// Check if the Model Path Exists
-		if (file_exists($modelPath))
-		{
-			// Require the Model
-			require_once ($modelPath);
-			
-			// Instantiate the Model
-			return new $modelName();
-		}
-	}
+    public function loadModel($modelName, $area = false)
+    {
+    	// Create the modelPath
+    	$modelPath = 'app/';
 	
-	// ------------------------------------------------------------------------
+	// Check if an Area has been defined
+	if ($area)
+	{
+	    // Create the modelPath
+	    $modelPath .= 'areas/' . $area . '/';
+	}
+		
+	// Create the modelPath
+	$modelPath .= 'models/'. $modelName . '.php';
+		
+	// Check if the Model Path Exists
+	if (file_exists($modelPath))
+	{
+	    // Require the Model
+	    require_once ($modelPath);
+			
+	    // Instantiate the Model
+	    return new $modelName();
+	}
+    }
+
+    // ------------------------------------------------------------------------
 	
 }
 /** eof */
