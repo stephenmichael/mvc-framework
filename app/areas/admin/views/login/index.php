fc7b4d7ff1f30f2b65ed805711d58d427ddcb07f
Stephen Michael <stephenmichael30@gmail.com>
2015-02-17 14:43:50 +0000

    Create index.php

---
+++ app/areas/admin/views/login/index.php
@@ -0,0 +1,7 @@
+<div class="row page-content">
+    <div class="col bp-1-5 col-centered">
+        <section>
+            <h1>Log In</h1>
+        </section>
+	</div><!--/ .col .bp-1-5 .col-centered -->
+</div><!--/ .row .page-content -->
