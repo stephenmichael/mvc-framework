f8e660f89fba059af771046ca1239313eca05c20
Stephen Michael <stephenmichael30@gmail.com>
2015-02-17 14:26:00 +0000

    Update index.php

--- app/views/blog/index.php
+++ app/views/blog/index.php
@@ -11,18 +11,18 @@ Html::getNav('blog/_nav', $viewData);
             <?php
             foreach($viewData['posts'] as $key => $value) {
                 echo '<article>';
-				echo '<header>';
+		echo '<header>';
                 echo '<p class="article-date">Posted by ' . $value->firstName . ' on ' . date("d F, Y", strtotime($value->publishedOn)) . '</p>';
                 echo '<h2 class="article-title">' . $value->title . '</h2>';
-				echo '</header>';
-			    echo Parsedown::instance()->parse($value->excerpt);
-				echo '<footer>';
-				echo '<div class="article-read-more">' . Html::actionLink('Read More <i class="fa fa-long-arrow-right"></i>', $value->slug, 'blog') . '</div>';
-				echo '</footer>';
+		echo '</header>';
+		echo Parsedown::instance()->parse($value->excerpt);
+		echo '<footer>';
+		echo '<div class="article-read-more">' . Html::actionLink('Read More <i class="fa fa-long-arrow-right"></i>', $value->slug, 'blog') . '</div>';
+		echo '</footer>';
                 echo '</article>';
                 echo '<div class="article-seperator"></div>';
-			}
-			?>
+	    }
+	    ?>
         </section>
     </div><!--/ .col .bp-2-8 .col-centered -->
 </div><!--/ .row .page-content -->
